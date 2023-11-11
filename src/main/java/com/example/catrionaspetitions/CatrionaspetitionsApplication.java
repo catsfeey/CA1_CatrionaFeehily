1767aeb3df28f3b2075a70cba7de14abbc2f9cbe
catsfeey <catrionafeehily@outlook.com>
2023-11-11 16:53:17 +0800

    Initial commit of project

---
+++ src/main/java/com/example/catrionaspetitions/CatrionaspetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.catrionaspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatrionaspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatrionaspetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/catrionaspetitions/CatrionaspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.catrionaspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatrionaspetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
