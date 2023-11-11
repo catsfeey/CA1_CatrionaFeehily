edc9cdc7cac95107b58611744d3d6ccd2e5f5fe2
catsfeey <catrionafeehily@outlook.com>
2023-11-11 17:55:33 +0800

    Second commit with create-petition html and Petition class

--- src/main/java/com/example/catrionaspetitions/ServletInitializer.java
+++ src/main/java/com/example/catrionaspetitions/ServletInitializer.java
@@ -2,6 +2,14 @@ package com.example.catrionaspetitions;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/")
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
@@ -9,5 +17,20 @@ public class ServletInitializer extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(CatrionaspetitionsApplication.class);
 	}
+
+	@GetMapping("/create")
+	public String showCreatePetitionForm() {
+
+		return "create-petition";
+	}
+
+	@PostMapping("/create")
+	public String createPetition(@RequestParam String title, @RequestParam String content) {
+		Petition petition = new Petition();
+		petition.setTitle(title);
+		petition.setContent(content);
+		return "redirect:/";
+	}
+
 
 }
